c111b7e3f70ea7a77767c77caab922ddfcf7a2f0
SavaSava <63306010+SavaSava1209@users.noreply.github.com>
2022-06-16 13:25:53 -0700

    check connection

--- server.js
+++ server.js
@@ -18,18 +18,19 @@ app.use(express.json())
 app.use(cors());
 
 app.get('/', (req, res) => res.send('working'))
-app.get('https://usa-state-map.herokuapp.com/filterState/:input', (req, res) => {
+app.get('/filterState/:input', (req, res) => {
     const { input } = req.params
+    console.log(input)
     return db("usa-state-map").select('state').from('location').where('state', 'like', `${input.toLowerCase()}%` ).then(data => res.status(200).json(data))
 })
 app.get('/:state', (req, res) => {
     const { state } = req.params        
-    fetch(`https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-state-boundaries&q=&facet=name&refine.name=${state.charAt(0).toUpperCase()+state.slice(1)}`)
-    .then(resp => resp.json()).then(data => {
+    // fetch(`https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-state-boundaries&q=&facet=name&refine.name=${state.charAt(0).toUpperCase()+state.slice(1)}`)
+    // .then(resp => resp.json()).then(data => {
 
-        res.json(data.records[0].fields.st_asgeojson.coordinates[0][0])
+    //     res.json(data.records[0].fields.st_asgeojson.coordinates[0][0])
 
-    }).catch(console.log)
+    // }).catch(console.log)
 })
 
 app.listen(process.env.PORT, () => {
