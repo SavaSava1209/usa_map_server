51148f523cdc7ceeeb4b8a55ebfac138852ab474
SavaSava <63306010+SavaSava1209@users.noreply.github.com>
2022-06-16 12:42:55 -0700

    check server

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
 const express = require('express')
 const cors = require('cors')
-
-const knex = require('knex')
+const knex = require('knex');
+const fetch = require('node-fetch');
 
 const db = knex({
     client: 'pg',
-    version: '7.2',
-    connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '',
-    database : 'usa_map'
+    connection: {     
+       connectionString: process.env.DATABASE_URL,
+       ssl: {
+           rejectUnauthorized: false
+       }
     }
-});  
+});
 
 const app = express()
 app.use(express.json())
 app.use(cors());
 
+app.get('/', (req, res) => res.send('working'))
+app.get('/filterState/:input', (req, res) => {
+    const { input } = req.params
+    return db("usa_map").select('state').from('location').where('state', 'like', `${input.toLowerCase()}%` ).then(data => res.status(200).json(data))
+})
 app.get('/:state', (req, res) => {
-    const { state } = req.params  
-    return db("usa_map").select('*').from('location').where('state', '=', state.toLowerCase()).returning('*')
-            .then(data => res.status(200).json(data) )
-            .catch(console.log)
+    const { state } = req.params        
+    fetch(`https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-state-boundaries&q=&facet=name&refine.name=${state.charAt(0).toUpperCase()+state.slice(1)}`)
+    .then(resp => resp.json()).then(data => {
+
+        res.json(data.records[0].fields.st_asgeojson.coordinates[0][0])
+
+    }).catch(console.log)
 })
 
-app.listen(3010, () => {
-    console.log('port is working on env')
+app.listen(process.env.PORT, () => {
+    console.log(`port is working on env ${process.env.PORT}`)
 })
 
 
